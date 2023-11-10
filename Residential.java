01db2abdb425c9f666cbfc055f8fb1c5f2de5385
Zarif Khan <100174834+Nicerice96@users.noreply.github.com>
2023-11-10 15:44:33 -0500

    First commit

--- Residential.java
+++ Residential.java
@@ -1,2 +1,13 @@
-public class Residential {
+public class Residential extends Site{
+
+    public Residential(int _units, double _rate) {
+        super(_units, _rate);
+    }
+
+    private double getBillableAmount(double _units, double _rate) {
+        double base = _units * _rate;
+        double tax = base * Site.TAX_RATE;
+        return base + tax;
+    }
+
 }
--- Site.java
+++ Site.java
@@ -1,2 +1,10 @@
 public class Site {
+    protected int _units;
+    protected double _rate;
+    public static final double TAX_RATE = 0.2;
+
+    public Site(int _units, double _rate) {
+        this._units = _units;
+        this._rate = _rate;
+    }
 }
