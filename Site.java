3ab0048b656ee8d28e0cdd5f3aa385868290fd8a
Zarif Khan <100174834+Nicerice96@users.noreply.github.com>
2023-11-13 09:53:04 -0500

    Extracted getBaseAmount() and getTaxAmount() methods

--- Site.java
+++ Site.java
@@ -1,10 +1,17 @@
-public class Site {
+public abstract class Site {
     protected int _units;
     protected double _rate;
     public static final double TAX_RATE = 0.2;
-
     public Site(int _units, double _rate) {
         this._units = _units;
         this._rate = _rate;
     }
+
+    public double getBillableAmount() {
+        return getBaseAmount() + getTaxAmount();
+    }
+
+    protected abstract double getTaxAmount();
+
+    protected abstract double getBaseAmount();
 }
